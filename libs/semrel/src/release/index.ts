b782bfb8bd48a5cd40bb6ab7e931440a1fa3242a
IKatsuba <katsuba.igor@gmail.com>
2021-01-13 10:41:49 +0300

    fix(semrel): correct github labels

--- libs/semrel/src/release/index.ts
+++ libs/semrel/src/release/index.ts
@@ -153,7 +153,10 @@ export async function runRelease(
             successComment: `:tada: This \${issue.pull_request ? 'pull request' : 'issue'} is included in version ${project}@\${nextRelease.version} :tada:
 
 The release is available on [GitHub release](<github_release_url>)`,
-            releasedLabels: `${project} is released<%= nextRelease.channel ? " on @\${nextRelease.channel}" : "" %>`
+            releasedLabels: [
+              `released<%= nextRelease.channel ? " on @\${nextRelease.channel}" : "" %>`,
+              project
+            ]
           }
         ]
       ]
