4feb38f1944562b5190384b773a53fadcfb9bc1c
IKatsuba <katsuba.igor@gmail.com>
2021-05-18 16:38:21 +0300

    feat(semrel): add the range option
    BREAKING CHANGE some note

--- libs/semrel/src/release/schema.ts
+++ libs/semrel/src/release/schema.ts
@@ -1,4 +1,5 @@
 import { JsonObject } from '@angular/compiler-cli/ngcc/src/packages/entry_point';
+import { BranchSpec } from 'semantic-release';
 
 export interface Schema extends JsonObject {
   publishable: boolean;
@@ -11,5 +12,5 @@ export interface Schema extends JsonObject {
         gitlabApiPathPrefix?: string;
         assets?: Array<{ path: string; label?: string }>;
       };
-  branches: Array<string | { name: string; prerelease?: boolean }>;
+  branches: Array<BranchSpec>;
 }
